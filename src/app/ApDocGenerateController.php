c4b1af07c4dd3125e60899efbc0faa075d237cf2
laililmahfud <laililmahvut@gmail.com>
2023-02-13 20:59:19 +0700

    generator

--- src/app/ApDocGenerateController.php
+++ src/app/ApDocGenerateController.php
@@ -5,8 +5,13 @@ use App\Http\Controllers\Controller;
 
 class ApDocGenerateController extends Controller{
        
+    public function __construct(
+        private $apDoc = new ApDoc
+    )
+    {
+    }
     public function index(){
-        (new ApDoc())->generate();
+        $this->apDoc->generate();
         return getdoc_file("/openapi-documentation.json");
     }
 }
